93aed896c00089e806022133f8810e4185c9a0f2
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-05-22 09:32:10 +1200

    Wrote function to render html template

--- rpi/listeners/email.py
+++ rpi/listeners/email.py
@@ -1,4 +1,6 @@
+import os
 import yagmail
+from jinja2 import Template, FileSystemLoader, Environment
 
 
 class EmailListener:
@@ -13,13 +15,28 @@ class EmailListener:
         if not event['water']:
             self.__send()
 
+    def __render_template(self):
+        # set up variables
+        template_file = './email.html'
+        render_vars = {
+                'name': 'Jordan',
+                'plant': 'Chili'
+                }
+
+        # get directory
+        script_path = os.path.dirname(os.path.abspath(__file__))
+
+        # render environment
+        env = Environment(loader=FileSystemLoader(script_path))
+        text = env.get_template(template_file).render(render_vars)
+
+        return text
+
+
     def __send(self):
         # structure email contents
         subject = 'Your plant needs watering!'
-        contents = """
-        Your plant is too dry and needs watering now. 
-        Regards, your friendly WaterBoy.
-        """
+        contents = self.__render_template()
 
         # send the email
         self.__yag.send(self.__dest, subject, contents)
