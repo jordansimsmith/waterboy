ec580348c52403af8bb376f8718025ced0bd0fa2
Jordan Sim-Smith <jordansimsmith@gmail.com>
2019-05-28 21:03:56 +1200

    Connected email listener to main script

--- rpi/waterboy.py
+++ rpi/waterboy.py
@@ -1,23 +1,48 @@
 #!/usr/bin/python
-import RPi.GPIO as GPIO
+import os
 import time
+import RPi.GPIO as GPIO
+from dotenv import load_dotenv
+from listeners.email import EmailListener
+
+# read .env file
+load_dotenv()
 
 # GPIO setup
 channel = 21
 GPIO.setmode(GPIO.BCM)
 GPIO.setup(channel, GPIO.IN)
 
+# list of registered listeners
+# notify(event) will be called on each listener
+listeners = []
+
+# register email listener
+username = os.environ['EMAIL_USERNAME']
+password = os.environ['EMAIL_PASSWORD']
+destination = os.environ['EMAIL_DESTINATION']
+email_listener = EmailListener(username, password, destination)
+listeners.append(email_listener)
+
 
 def callback(channel):
-    # TODO: notify listeners here
-    if GPIO.input(channel):
-        print("Water detected!")
+
+    water = not GPIO.input(channel)
+    if water:
+        print('Water detected.')
     else:
-        print("No water detected!")
+        print('No water detected.')
+
+    # structure event
+    event = {'water': water}
+
+    # notify listeners
+    for l in listeners:
+        l.notify(event)
 
 
 # Add event listener
-GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=300)
+GPIO.add_event_detect(channel, GPIO.BOTH, bouncetime=1000)
 GPIO.add_event_callback(channel, callback)
 
 # Listen indefinitely
